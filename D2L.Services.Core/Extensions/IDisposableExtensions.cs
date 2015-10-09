47fdd609a3441572cb13bc4ff5914ba21c164db1
Jacob Parker <Jacob.Parker@D2L.com>
2015-10-09 17:40:21 -0400

    Import D2L.Services.Core.Extensions

--- D2L.Services.Core/Extensions/IDisposableExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System {
-	public static class IDisposableExtensions {
-		public static void SafeDispose( this IDisposable @this ) {
-			if( @this != null ) {
-				@this.Dispose();
-			}
-		}
-	}
-}
