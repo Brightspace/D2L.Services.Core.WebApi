bfe6a564a1e6806864307a66bbd4cbce7d481a01
Jacob Parker <Jacob.Parker@D2L.com>
2015-11-30 12:59:09 -0500

    Standardizing on layout of extension methods

--- D2L.Services.Core/Extensions/System.Web.Http.Dependencies.IDependencyScope.Extensions.cs
+++ D2L.Services.Core/Extensions/System.Web.Http.Dependencies.IDependencyScope.Extensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web.Http.Dependencies;
 
-namespace D2L {
-	public static partial class _D2LServicesCore_Extensions {
+namespace D2L.Services {
+	public static partial class __D2LServicesCore_Extensions {
 		// TODO: move to D2L.Services.Core.Extensions and make public? Downsides: people shouldn't have to use this.
 		internal static T Get<T>( this IDependencyScope @this ) {
 			return (T)@this.GetService( typeof( T ) );
